faec14c165eb4d7590c34e343c9e45794c58d242
shanghanrun <idim7@naver.com>
2023-08-14 18:15:03 +0900

    회원가입 구현하기

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')), # pybo/로 시작되는 페이지 요청은 pybo/url.py 파일내용을 참조 처리
     # path('pybo/', views.index),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  #  루트('/')페이지에 해당하는 것
 
 ]
