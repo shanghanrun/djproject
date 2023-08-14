6257303865272e027f379ae4ad6ee3923fa29e9d
shanghanrun <idim7@naver.com>
2023-08-14 16:55:37 +0900

    03-2 페이지네이션

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,13 +4,20 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from .models import Question
 
+from django.core.paginator import Paginator
+
 
 def index(request):
     """
     pybo 목록 출력
     """
+    page = request.GET.get('page', '1') # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
