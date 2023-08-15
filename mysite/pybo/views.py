11ba2d41294d3ee2e09d884952d8f1c45818d6e1
shanghanrun <idim7@naver.com>
2023-08-15 11:22:55 +0900

    모델에 글쓴이 추가하기

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -5,6 +5,7 @@ from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -29,7 +30,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
-
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -39,6 +40,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -48,7 +50,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
-
+@login_required(login_url='common:login')
 def question_create(request):
     """
     pybo 질문등록
@@ -57,6 +59,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
